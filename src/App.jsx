602d2abee3b8bfe00aae0065e023a3421187525b
Sangam Thapa <swngam26@gmail.com>
2025-06-05 13:04:03 +0545

    UI Part Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './index.css';
+import Weather from './components/Weather';
 
 const App = () => {
   return (
     <div className='app'>
 
-      
+      <Weather />
     </div>
   )
 }
